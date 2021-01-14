ab0bae2421a8d06f3bd776332cc769caf802e23c
KangDroid <hyunwoo286@gmail.com>
2021-01-14 23:35:30 +0900

    Dashboard: Magnify
    KDNS-11 #done

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,25 +1,21 @@
 const main = {
     init: function () {
-        const _this = this;
-
-        // Click Listener
-        $('#notification_table').on('click', '.btn-select' , function () {
-            var row = $(this).closest('tr');
-            const id = row.find('td:eq(0)').text();
-
+        $('.card-link').on('click', function() {
+            var id = $(this).attr('id');
             $.ajax({
                 type: 'DELETE',
                 url: '/delete/notifDelete/' + id,
                 dataType: 'json',
                 contentType: 'application/json; charset-utf-8'
             }).done(function() {
-                alert('Removed ID: ' + id)
+                alert("Removed Notification ID: " + id);
                 window.location.href = '/';
             }).fail(function(error) {
                 alert(JSON.stringify(error));
             });
         });
-        $('#clear-all').on('click', function() {
+
+        $('.card-link-all').on('click', function() {
             $.ajax({
                 type: 'DELETE',
                 url: '/delete/notifDelete/all',
@@ -30,7 +26,7 @@ const main = {
             }).fail(function(error) {
                 alert(JSON.stringify(error));
             })
-        })
+        });
     }
 };
 
